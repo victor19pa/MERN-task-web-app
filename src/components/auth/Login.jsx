6629c0df2d56aea7c80b9f2cef3511f43bedce63
victor19pa <vpinedaa@unah.hn>
2022-05-01 20:02:12 -0600

    :sparkles: feat: login form layout

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 
 const Login = () => {
+  const onChange = () => {};
+
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+
+        <form>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Correo Electronico"
+              onChange={onChange}
+              autoComplete={false}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Contraseña"
+              onChange={onChange}
+              autoComplete={false}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
