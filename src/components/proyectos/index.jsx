0068c92bdf8f9dba83424ab6b1b067c9a30b2a86
victor19pa <vpinedaa@unah.hn>
2022-05-07 22:41:13 -0600

    :sparkles: feat: listando proyectos agregados

--- src/components/proyectos/index.jsx
+++ src/components/proyectos/index.jsx
@@ -1,2 +1,4 @@
 export { default as Proyectos } from "./Proyectos";
 export { default as NuevoProyecto } from "./NuevoProyecto";
+export { default as ListadoProyectos } from "./ListadoProyectos";
+export { default as Proyecto } from "./Proyecto";
