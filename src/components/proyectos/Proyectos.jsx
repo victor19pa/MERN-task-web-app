7d85e599ef9f25be14c3760405bcf70de5189bb7
victor19pa <vpinedaa@unah.hn>
2022-05-07 22:57:39 -0600

    :sparkles: feat: creando formulario tareas

--- src/components/proyectos/Proyectos.jsx
+++ src/components/proyectos/Proyectos.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Barra from "../layout/Barra";
 import Sidebar from "../layout/Sidebar";
+import FormTarea from "../tareas/FormTarea";
 
 const Proyectos = () => {
   return (
     <div className="contenedor-app">
       <Sidebar />
-
       <div className="seccion-principal">
         <Barra />
-
         <main>
+          <FormTarea />
           <div className="contenedor-tareas">{/* <ListadoTareas /> */}</div>
         </main>
       </div>
