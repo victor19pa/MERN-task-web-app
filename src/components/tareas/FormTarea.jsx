33a069bc12cf3e810e7f7bc0a339b56b5a8cf7a6
victor19pa <vpinedaa@unah.hn>
2022-05-08 20:07:57 -0600

    :sparkles: feat: insertando tareas en proyecto actual

--- src/components/tareas/FormTarea.jsx
+++ src/components/tareas/FormTarea.jsx
@@ -1,14 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ProyectoContext from "../../context/Proyectos/ProyectoContext";
+import TareaContext from "../../context/Tareas/TareaContext";
 
 const FormTarea = () => {
   const { proyecto } = useContext(ProyectoContext);
+  const { agregarTareas } = useContext(TareaContext);
+
+  const [tarea, setTarea] = useState({
+    nombre: "",
+  });
+
+  const { nombre } = tarea;
+
   //sino hay proyecto seleccionado
   if (!proyecto) {
     return null;
   }
   //array destructuring para extraer actual
   const [proyectoActual] = proyecto;
+
+  const handleChange = (e) => {
+    setTarea({
+      ...tarea,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -18,7 +34,9 @@ const FormTarea = () => {
     //pasar validacon
 
     //agregar nueva tarea
-
+    tarea.proyectoId = proyectoActual.id;
+    tarea.estado = false;
+    agregarTareas(tarea);
     //reiniciar form
   };
 
@@ -31,6 +49,8 @@ const FormTarea = () => {
             className="input-text"
             placeholder="Nombre Tarea"
             name="nombre"
+            value={nombre}
+            onChange={handleChange}
           />
         </div>
         <div className="contenedor-input">
